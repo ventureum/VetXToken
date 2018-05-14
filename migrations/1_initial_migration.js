6de8f0cbfe651731d667fbcb3c2cfe6277869a1f
Timothy Wang <timothywangdev@gmail.com>
2018-05-14 06:15:49 +0000

    Simplify migration procedure

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Migrations = artifacts.require('./Migrations.sol')
+var Token = artifacts.require('./VetXToken.sol')
+var SafeMath = artifacts.require('./SafeMath.sol')
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
+module.exports = async function (deployer) {
+  // await deployer.deploy(Migrations)
+  deployer.deploy(SafeMath)
+  deployer.link(SafeMath, Token)
+  deployer.deploy(Token, '1000000000000000000000000000', 'VetX', 18, 'VTX')
+}
--- migrations/2_migrate_tokens.js
+++
@@ -1,5 +0,0 @@
-var Token = artifacts.require("./VetXToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Token, "1000000000000000000000000000", "VetX", 18, "VTX");;
-};
